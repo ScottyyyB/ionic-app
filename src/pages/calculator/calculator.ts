61e2ce68f8f6c4a69402b5435e9c2855f0009d7e
ScottB <17sb@cis.dk>
2017-12-12 17:31:49 +0100

    Add ability to switch to Imperial

--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -14,9 +14,66 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'calculator.html',
 })
 export class CalculatorPage {
+  height: number;
+  weight: number;
+  imperialHeight: number;
+  imperialWeight: number;
+  bmiValue: number;
+  bmiImperialValue: number;
+  bmiMessage: string;
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
+
+  imperial: boolean = false;
+  bmiSwitch() {
+    if (!this.imperial) {
+      this.imperial = true;
+      return;
+    }
+    if (this.imperial) {
+      this.imperial = false;
+      return;
+    }
+  }
+
+  calculateBMI() {
+    if (this.weight > 0 && this.height > 0 && !this.imperial) {
+      let finalBmi = this.weight / (this.height / 100 * this.height / 100);
+      this.bmiValue = parseFloat(finalBmi.toFixed(2));
+      this.setBMIMessage();
+    }
+
+    if (this.imperialWeight > 0 && this.imperialHeight > 0 && this.imperial) {
+      let finalBmi = this.imperialWeight * 703 / (this.imperialHeight * this.imperialHeight);
+      this.bmiValue = parseFloat(finalBmi.toFixed(2));
+      this.setBMIMessage();
+    }
+  }
+
+  private setBMIMessage() {
+
+    if (this.bmiValue < 18.5) {
+      this.bmiMessage = 'Underweight'
+      return;
+    }
+
+    if (this.bmiValue < 25) {
+      this.bmiMessage = 'Normal';
+      return;
+    }
+
+    if (this.bmiValue < 30) {
+      this.bmiMessage = 'Overweight';
+      return;
+    }
+
+    else {
+      this.bmiMessage = 'Obese';
+    }
+  }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CalculatorPage');
